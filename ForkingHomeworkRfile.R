c8160bea1f81ae7b55544514ee1d4a3865e0cb2a
Mary-Almah <112513266+md5258a@users.noreply.github.com>
2023-04-20 19:49:56 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,6 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+diamonds[cut == "Premium" & price > 7000 & price < 10000, 
+         by = .(carat, cut, price)][order(-carat)][c(1:20)]
